bad4f34ec56775d4a60f7b6491798e7a26fe9ef1
kapipec <vanya1996ryabiko@gmail.com>
2021-08-07 11:01:21 +0300

    simple tasks

---
+++ js_base/assets/js/index.js
@@ -0,0 +1,24 @@
+let s1='hello',
+    s2='Vasya';
+let res;
+res=s1+" "+s2;
+console.log(res);
+
+let x1=5,
+    x2=10;
+res = x1+ x2;
+console.log(res);
+console.log('result is', res);
+alert(`result is ${res}`);
+
+let x = 'hello js';
+let a = 100;
+let b = 5.5;
+let c = true;
+const COLOR='RedColor';
+console.log(x,a,b,c,COLOR);
+
+let z = prompt("Vvedite znachenie A dlya formuly s=1/2*a*h", 1);
+let h = prompt("Vvedite znachenie H dlya formuly s=1/2*a*h", 1);
+res=z*h*0.5;
+console.log(res);
